dfe398e3c3d8fd402d3aed21e3c300fc6f37337d
Freddy Daniel Valencia Medina <valenciamedinafreddydaniel1@gmail.com>
2025-06-08 11:43:05 -0400

    Primer commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+// Lee las propiedades desde local.properties de la raíz
+val localProperties = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) {
+        file.inputStream().use { this.load(it) }
+    }
+}
+
+android {
+    namespace = "com.example.nutriton"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.nutriton"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        buildConfigField("String", "NUTRITION_APP_ID", "\"${localProperties.getProperty("NUTRITION_APP_ID") ?: ""}\"")
+        buildConfigField("String", "NUTRITION_APP_KEY", "\"${localProperties.getProperty("NUTRITION_APP_KEY") ?: ""}\"")
+        buildConfigField("String", "RECIPE_APP_ID", "\"${localProperties.getProperty("RECIPE_APP_ID") ?: ""}\"")
+        buildConfigField("String", "RECIPE_APP_KEY", "\"${localProperties.getProperty("RECIPE_APP_KEY") ?: ""}\"")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
